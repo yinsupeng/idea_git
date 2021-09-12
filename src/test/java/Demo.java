ddc8522830b7d726f4f201f3e45a7d57bcde1d2b
yinsupeng <yin_supeng@163.com>
2021-09-12 23:28:11 +0800

    first commit

---
+++ src/test/java/Demo.java
@@ -0,0 +1,12 @@
+/**
+ * @author spyin
+ * @title: Demo
+ * @projectName idea_git
+ * @description: TODO
+ * @date 2021/9/1223:22
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("这是第一个程序");
+    }
+}
